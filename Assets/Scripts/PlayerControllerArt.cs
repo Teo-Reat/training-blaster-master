5a8b54058e838415ca10cf95540d3e36f88846e1
ArtAkaArt <91897648+ArtAkaArt@users.noreply.github.com>
2023-06-11 18:49:26 +0500

    Upd

--- Assets/Scripts/PlayerControllerArt.cs
+++ Assets/Scripts/PlayerControllerArt.cs
@@ -1,95 +1,102 @@
 using System;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Vector3 = UnityEngine.Vector3;
 
 [RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
 public class PlayerControllerArt : MonoBehaviour
 {
+    [Header("Movement")] 
     [SerializeField] private float movementSpeed;
-    [SerializeField] private float jumpimgForce;
+    [SerializeField] private int acceletarion = 50;
+    [SerializeField] private int deceleration = 50;
+
+    [Header("Ground Parameters")]
+    [SerializeField, Range(0, 1)] private float MinGroundNormalY = .65f;
+    [SerializeField] private Transform raycastPoint;
+    [SerializeField] private float stepSmooth = .1f;
+
+    [Header("Jumping")] [SerializeField] private float jumpimgForce;
     [SerializeField] private float gravityForce;
-    [SerializeField] private float MinGroundNormalY = .65f;
-    [SerializeField] private bool isDashing;
+    [SerializeField] private bool sealInput;
 
+    [Header("Debug unview later")]
+    [SerializeField] private float input;
+    [SerializeField] private bool onGround;
+    [SerializeField] private bool onSlope;
 
-    private float minMoveDistance = .001f;
-    private bool onGround;
-    private bool onSlope;
+    private Vector3 inputDirection;
     private Rigidbody playerRb;
     private CapsuleCollider playerCollider;
-    private Vector3 targetVelocity; //���� ����� ���������
-    private Vector3 Velocity; // ���� �������� ������
     private Vector3 groundNormal;
-    private float shellRadius = .02f;
-    private float jumpForceMax;
 
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerCollider = GetComponent<CapsuleCollider>();
-        playerRb.useGravity = false;
     }
 
     void Update()
     {
-        targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-        if (Input.GetKeyDown(KeyCode.Space) && onGround)
-            Velocity.y = jumpimgForce;
+        input = Input.GetAxisRaw("Horizontal");
+        inputDirection = new Vector3(input, 0, 0);
+
+        if (!Input.GetKeyDown(KeyCode.Space) || !onGround) return;
+        playerRb.velocity = new Vector3(playerRb.velocity.x, 0, 0);
+        playerRb.AddForce(Vector3.up * jumpimgForce, ForceMode.Impulse);
     }
 
     void FixedUpdate()
     {
-        Velocity += Physics.gravity * (gravityForce * Time.deltaTime);
-
-        Velocity.x = targetVelocity.x * movementSpeed;
-        onGround = false;
-        onSlope = false;
-        jumpForceMax = (jumpimgForce - 1) * Time.fixedDeltaTime;
-        transform.LookAt(transform.position + new Vector3(targetVelocity.x, 0, 0));
-
-        var deltaPosition = Velocity * Time.fixedDeltaTime;
-        var moveAlongGround = new Vector3(groundNormal.y, -groundNormal.x);
-        var move = moveAlongGround * deltaPosition.x;
-
-        Movement(move, false);
-        move = Vector3.up * deltaPosition.y;
-        Movement(move, true);
+        CheckGround();
+        CheckForwardDirection();
+        AddGravity();
+        MoveCharacter();
     }
 
-    void Movement(Vector3 direction, bool isYDirection)
+    private void AddGravity()
     {
-        var tryingToJump = direction.y > jumpForceMax;
-        var isHitAngleOk = true;
-        var distance = direction.magnitude; // ����������
-        if (distance > minMoveDistance)
-        {
-            var directionOnYMove = !tryingToJump && isYDirection ? Vector3.down : direction;
-            var isHitted = Physics.Raycast(transform.position, directionOnYMove, out var hit,
-                (isYDirection ? .1f : playerCollider.radius) + shellRadius);
-            if (isHitted)
-            {
-                var currentNormal = hit.normal;
-                isHitAngleOk = currentNormal.y > MinGroundNormalY;
-                if (currentNormal.y > MinGroundNormalY)
-                {
-                    onGround = true;
-                    if (isYDirection)
-                    {
-                        onSlope = currentNormal.y != 1;
-                        groundNormal = currentNormal;
-                        currentNormal.x = 0;
-                    }
+        if (!onGround && playerRb.velocity.y <= 0)
+            playerRb.velocity += gravityForce * Time.deltaTime * Physics.gravity;
+    }
 
-                    var projection = Vector3.Dot(Velocity, currentNormal);
-                    if (projection < 0)
-                        Velocity -= projection * currentNormal;
-                    var mod = hit.distance - shellRadius;
-                    distance = mod < distance ? mod : distance;
-                }
-            }
+    private void MoveCharacter()
+    {
+        if (sealInput && !onGround) return;
+        var targetSpeed = input * movementSpeed;
+        var deltaSpeed = targetSpeed - playerRb.velocity.x;
+        var directionAlongGround = Vector3.ProjectOnPlane(Vector3.right, groundNormal);
+        var accelRate = input == 0 ? deceleration : acceletarion;
+        playerRb.AddForce(directionAlongGround * (accelRate * deltaSpeed), ForceMode.Force);
+        transform.LookAt(transform.position + inputDirection);
+    }
+
+    private void CheckGround()
+    {
+        var isHitting = Physics.Raycast(transform.position, Vector3.down, out var collision, .1f);
+        groundNormal = collision.normal;
+        onGround = isHitting;
+        onSlope = onGround && groundNormal.y != 1;
+        playerRb.useGravity = !onSlope;
+    }
+
+    private void CheckForwardDirection()
+    {
+        var isHittingLower = Physics.Raycast(transform.position, transform.forward, out var collisionLower,
+            playerCollider.radius);
+        if (!isHittingLower) return;
+        var isHittingUpper = Physics.Raycast(raycastPoint.position, transform.forward, out var collisionUpper,
+            playerCollider.radius);
+        if (!isHittingUpper)
+        {
+            if (!onSlope && input != 0)
+                playerRb.position += new Vector3(0, stepSmooth, 0);
+            return;
         }
 
-        if ((onSlope && isYDirection && !tryingToJump) || (!isHitAngleOk && !isYDirection)) return;
-        playerRb.position += direction.normalized * distance;
+        if (Mathf.Abs(collisionUpper.normal.y) < MinGroundNormalY)
+            input = 0;
     }
 }
